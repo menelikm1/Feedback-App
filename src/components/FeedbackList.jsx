3985ab1ab678c20da61a5994e513060093e2d062
Menelik Melesse <menelikmelesse@Meneliks-MacBook-Pro-2.local>
2022-02-27 16:30:28 -0500

    Initial Commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,46 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import Feedbackitem from "./Feedbackitem"
+import FeedbackContext from '../context/Feedbackcontext'
+
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+    if (!feedback || feedback.length === 0) {
+        return <p>No feedback yet</p>
+    }
+
+    return (
+    <div className="feedback-list">
+        <AnimatePresence>
+            {feedback.map((item) => (
+            <motion.div 
+                key={item.id}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+            >
+                <Feedbackitem 
+                key={item.id} 
+                item={item} 
+                />
+            </motion.div>
+        ))}
+        </AnimatePresence>
+    </div>
+  )
+}
+
+//   return (
+//     <div className="feedback-list">
+//         {feedback.map((item) => (
+//             <Feedbackitem 
+//             key={item.id} 
+//             item={item} 
+//             handleDelete={handleDelete}/>
+//         ))}
+//     </div>
+//   )
+// }
+
+
+export default FeedbackList
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+            id: 1,
+            rating: 10,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur lectus ut lacinia venenatis. Aliquam lacus ex, convallis eget eleifend eu, iaculis vitae arcu.'
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur lectus ut lacinia venenatis. Aliquam lacus ex, convallis eget eleifend eu, iaculis vitae arcu.'
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur lectus ut lacinia venenatis. Aliquam lacus ex, convallis eget eleifend eu, iaculis vitae arcu.'
+        }
+]
+
+export default FeedbackData
